29f158dfa47f0107efef789b37f65d8f3f16f0ce
Mikkely <mikkely.espinoza@gmail.com>
2023-10-30 12:43:48 -0700

    db fix

--- db/db.sql
+++ db/db.sql
@@ -1,17 +1,17 @@
 CREATE TABLE OFICINA(
 id_oficina int not null auto_increment,
-clave_oficina Varchar(100) Not null,
+clave_oficina text Not null,
 Primary key(id_oficina)
 );
 
 CREATE TABLE PRIMA_PENDIENTE(
-id_fianza Varchar(100) Not null,
+id_fianza serial Not null,
 id_oficina int Not null,
-nombre_fiado Varchar(100) Not null,
-tipo_movimiento Varchar(100) Not null,
+nombre_fiado text Not null,
+tipo_movimiento text Not null,
 antiguedad int not null,
-dias_vencimiento int not null,
 importe int not null,
-Primary key(id_fianza),
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+Primary key (id_fianza),
  Foreign key (id_oficina) REFERENCES OFICINA(id_oficina)
 );
