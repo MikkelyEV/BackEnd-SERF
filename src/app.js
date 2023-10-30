a4f5a02bd4fa95868b2d4178f56cd0d8bca71f73
Mikkely <mikkely.espinoza@gmail.com>
2023-10-30 15:46:39 -0700

    fix oficinas

--- src/app.js
+++ src/app.js
@@ -6,16 +6,16 @@ import { PORT } from './config.js'
 const app = express();
 
 app.get('/oficinas', async (req, res) => {
-    const [oficinas] = await conn.query(`SELECT * AS OFICINAS FROM OFICINA`);
+    const [oficinas] = await conn.query(`SELECT * FROM OFICINA AS OFICINAS`);
     console.log(oficinas)
-    res.send(oficinas[0])
+    res.send(oficinas)
 })
 
 app.get('/primas_pendientes', async (req, res) => {
     const oficina = req.body.id_oficina;
-    const [primas_pendientes] = await conn.query(`SELECT * AS PRIMAS_PENDIENTES FROM PRIMA_PENDIENTE WHERE ID_OFICINA=${oficina}`);
+    const [primas_pendientes] = await conn.query(`SELECT * FROM PRIMA_PENDIENTE WHERE ID_OFICINA=${oficina}`);
     console.log(primas_pendientes)
-    res.send(result[0])
+    res.send(primas_pendientes)
 })
 
 app.listen(PORT)
