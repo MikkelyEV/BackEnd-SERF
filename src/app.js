c7d52a7adc4cef179d090a57d8b88748bde7aac7
Mikkely <mikkely.espinoza@gmail.com>
2023-10-31 19:42:49 -0700

    query fix

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.get('/oficinas', async (req, res) => {
 })
 
 app.get('/primas_pendientes', async (req, res) => {
-    const oficina = req.body.id_oficina;
+    const oficina = req.query.id_oficina;
     const [primas_pendientes] = await conn.query(`SELECT * FROM PRIMA_PENDIENTE3 WHERE ID_OFICINA=${oficina}`);
     console.log(primas_pendientes)
     res.send(primas_pendientes)
